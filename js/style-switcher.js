ebb4a2741375a614522fdda93ecc28b7f11e2286
Jim de Lange <0344960@student.rocvantwente.nl>
2025-01-24 20:54:27 +0100

    Portfolio project toegevoegd

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,11 +1,10 @@
 const styleSwitcherToggle = document.querySelector('.style-switcher-toggler');
-
 styleSwitcherToggle.addEventListener('click', () => {
     document.querySelector('.style-switcher').classList.toggle('open');
 })
 
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
     if(document.querySelector('.style-switcher').classList.contains('open'))
     {
         document.querySelector('.style-switcher').classList.remove('open');
